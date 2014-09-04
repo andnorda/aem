8fdfa94cfcb6fc433379ef63cae5fdc438cd7853
Andreas Nordahl <andnorda@gmail.com>
2014-09-04 09:41:42 +0200

    A simple test and maven dependencies

--- content/src/main/content/jcr_root/apps/training/components/listchildren/ListChildren.java
+++ content/src/main/content/jcr_root/apps/training/components/listchildren/ListChildren.java
@@ -9,15 +9,19 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ListChildren extends WCMUse {
-    private List<Page> items = new ArrayList<Page>();
+    private final List<Page> items = new ArrayList<Page>();
 
     public void activate() throws Exception {
-        final String path = getProperties().get("pagepath", getCurrentPage().getAbsoluteParent(2).getPath());
+        final String defaultPath = getCurrentPage()
+                .getAbsoluteParent(2)
+                .getPath();
+        final String path = getProperties()
+                .get("pagepath", defaultPath);
         final Page rootPage = getPageManager().getPage(path);
 
         if (rootPage != null) {
-            Iterator<Page> childPages = rootPage.listChildren(new
-                    PageFilter(getRequest()));
+            Iterator<Page> childPages = rootPage.listChildren(
+                    new PageFilter(getRequest()));
             while (childPages.hasNext()) {
                 items.add(childPages.next());
             }
---
+++ content/src/test/java/com/mycompany/myproject/ListChildrenTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.myproject;
+
+import apps.training.components.listchildren.ListChildren;
+import org.junit.Test;
+
+public class ListChildrenTest {
+    @Test
+    public void shouldFail() throws Exception {
+        new ListChildren();
+    }
+}
